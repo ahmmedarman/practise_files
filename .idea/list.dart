793287ce6d0892f4e8e81c01bd7702b551cd14e4
rasul <rasulahamed132@gmail.com>
2024-02-17 21:12:00 +0600

    change

---
+++ .idea/list.dart
@@ -0,0 +1,28 @@
+ void main(){
+  /*List<String> studentList = ['Arman', 'Prova', 'Supty'];
+   print(studentList);
+   print(studentList.reversed);
+   print(studentList.length);
+   studentList.add('soncoy');
+   studentList.add('mithu');
+  print(studentList);
+  print(studentList[0]);
+  studentList=['sabbir'];
+ print(studentList);
+  studentList.addAll(['tasnim','taskin','riyan']);
+  print(studentList);
+  studentList.insert(1, 'israt',);
+  print(studentList);
+  studentList.insertAll(2, ['raisa','arisha']);
+  print(studentList);
+  */
+
+
+  mMap <int,String> studentName = {
+   1 : 'Arman',
+   2 : 'supty'
+    arman
+};
+print(studentName);
+
+}
---
+++ bin/practise_files.dart
@@ -0,0 +1,23 @@
+  void main() {
+   /* firstNumber = 40;
+  int secondNumber = 30;
+  int addition = firstNumber+secondNumber;
+  int subtraction = firstNumber-secondNumber;
+  int multipication = firstNumber*secondNumber;
+  int modulus = firstNumber%secondNumber;
+  double divide = firstNumber / secondNumber;
+
+  print(addition);
+  print(subtraction);
+  print(multipication);
+  print(modulus);
+  print(divide);*/
+
+
+   /*String myName = 'rasul ahmmed arman';
+
+   print(myName);*/
+    int x = 10;
+    int y= 3;
+    print(x/y);
+  }
