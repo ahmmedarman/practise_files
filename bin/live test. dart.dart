5c8d734084382248177899d93dc0459f356a0d7a
rasul <rasulahamed132@gmail.com>
2024-03-16 00:10:52 +0600

    change

---
+++ bin/live test. dart.dart
@@ -0,0 +1,9 @@
+void main(){
+  List<String>listFruits=['Name','Color','Price'];
+  List<Map<String,dynamic>>fruits =[
+    Name:'Apple',Color:'Red',Price:$2.5;
+
+  Name:'Banana',Color:'Yellow',Price:$i1.0;
+  Name: 'Grapes', Color: 'Purple', Price: $3.0;
+  ];
+}
